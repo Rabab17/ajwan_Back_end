8dc8988dcff3b31286bcdc70186eaa75b9146f04
rabab alaa <161600491+Rabab17@users.noreply.github.com>
2025-08-06 16:59:52 +0300

    Before Strapi upgrade

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = ({ env }) => ({
   documentation: {
     enabled: true,
     config: {
@@ -10,5 +10,25 @@ module.exports = {
       },
     },
   },
-};
+
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+        
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+      
+    },
+  },
+  
+});
+
+
 
